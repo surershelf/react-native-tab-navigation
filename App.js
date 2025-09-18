6834a08f5011b9916a3ab4747d6bb92b21496509
mazzo <mazzolucas0@gmail.com>
2025-09-17 22:36:13 -0300

    Terminando o trabalho

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="CASA TELA" component={HomeScreen} />
+        <Tab.Screen name="CONFIGURAÇÕES" component={ConfigScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "arial",
+    fontSize: 20,
+  },
+  text: {
+    textAlign: "center",
+    color: "black",
+    fontFamily: "arial",
+    fontSize: 20,
   },
 });
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.text}>
+        CASINHA TELA (TRADUÇÃO BR) SEM SOBERANIA DOS EUA🤬🤬
+      </Text>
+    </View>
+  );
+}
+function ConfigScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.text}>CONFIGURAÇÕES ✅✅✅</Text>
+    </View>
+  );
+}
